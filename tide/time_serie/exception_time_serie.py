9e8c805d3e5c7e68dd524cbdf57c46794fea2f20
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-13 10:30:29 -0500

    ajout des fonctions get_data_gaps

--- tide/time_serie/exception_time_serie.py
+++ tide/time_serie/exception_time_serie.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
+from datetime import timedelta
 
 import pandas as pd
 
-from .time_serie_dataframe import get_data_gaps_message
+from .time_serie_models import DataGapPeriod
 
 
 @dataclass(frozen=True)
@@ -26,6 +27,38 @@ class WaterLevelDataGapError(Exception):
             f"Il y a des périodes de données manquantes qui excède la limite permise de {self.max_time_gap} pour la"
             f" station {self.station_id}. {get_data_gaps_message(gaps=self.gaps)}"
         )
+
+
+def get_data_gap_periods(gaps: pd.DataFrame) -> list[DataGapPeriod]:
+    """
+    Récupère les périodes de données manquantes.
+
+    :param gaps: (pd.DataFrame) DataFrame contenant les périodes de données manquantes.
+    :return: (list[DataGap]) Périodes de données manquantes.
+    """
+    return [
+        DataGapPeriod(
+            start=row["event_date"] - row["data_time_gap"], end=row["event_date"]
+        )
+        for index, row in gaps.iterrows()
+    ]
+
+
+def get_data_gaps_message(gaps: pd.DataFrame) -> str:
+    """
+    Journalise les périodes de données manquantes.
+
+    :param gaps: (pd.DataFrame) Périodes de données manquantes.
+    :return: (str) Journalisation des périodes de données manquantes.
+    """
+    data_gaps_list: list[DataGapPeriod] = get_data_gap_periods(gaps=gaps)
+
+    total_duration_minutes = (
+        sum((gap.duration for gap in data_gaps_list), timedelta()).total_seconds() / 60
+    )
+    gaps_str = "; ".join([str(gap) for gap in data_gaps_list])
+
+    return f"{total_duration_minutes} minutes de données manquantes pour les périodes suivantes : {gaps_str}."
 
 
 class InterpolationValueError(Exception):
