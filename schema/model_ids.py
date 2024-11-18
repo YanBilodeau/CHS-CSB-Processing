8ee9790ae4f5ec1fcbfe5bb9eb1529130284fef8
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-18 09:44:43 -0500

    commit initial

---
+++ schema/model_ids.py
@@ -0,0 +1,5 @@
+# Constantes pour les noms des colonnes des données du schéma de données DataLoggerSchema.
+TIME_UTC: str = "Time_UTC"
+DEPTH_METER: str = "Depth_meter"
+LONGITUDE_WGS84: str = "Longitude_WGS84"
+LATITUDE_WGS84: str = "Latitude_WGS84"
