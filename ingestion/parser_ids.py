324164523618bce8ce3606d23e6c32be9e5bca75
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-18 09:44:31 -0500

    ajout de commentaires

--- ingestion/parser_ids.py
+++ ingestion/parser_ids.py
@@ -1,17 +1,15 @@
-TIME_UTC: str = "Time_UTC"
-DEPTH_METER: str = "Depth_meter"
-LONGITUDE_WGS84: str = "Longitude_WGS84"
-LATITUDE_WGS84: str = "Latitude_WGS84"
-
+# Constantes pour les noms des colonnes des données OFM et DCDB.
 LON: str = "LON"
 LAT: str = "LAT"
 DEPTH: str = "DEPTH"
 TIME: str = "TIME"
 
+# Constantes pour les noms des colonnes des données Lowrance.
 LONGITUDE_LOWRANCE: str = "Longitude[°WGS84]"
 LATITUDE_LOWRANCE: str = "Latitude[°WGS84]"
 DEPTH_LOWRANCE: str = "WaterDepth[Feet]"
 TIME_LOWRANCE: str = "DateTime[UTC]"
 
+# Constantes pour les dtypes des données.
 FLOAT64: str = "float64"
 EPSG_WGS84: str = "EPSG:4326"
