dd98714f9b249797fc31d25d560fac98c42654a1
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-18 16:08:11 -0500

    ajout de détail dans le log

--- ingestion/parser_abc.py
+++ ingestion/parser_abc.py
@@ -18,7 +18,7 @@ from schema import (
     LATITUDE_WGS84,
 )
 
-LOGGER = logger.bind(name="CSB-Pipeline.Ingestion.Parser")
+LOGGER = logger.bind(name="CSB-Pipeline.Ingestion.Parser.ABC")
 
 
 @dataclass
@@ -41,16 +41,19 @@ class DataParserABC(ABC):
         :raises ParsingDataframeTimeError: Erreur si la colonne de temps est absente.
         """
         LOGGER.debug(
-            f"Validation des colonnes du dataframe : {[column.column_name for column in column_exceptions]}."
+            f"Validation des colonnes {[column_.column_name for column_ in column_exceptions]} du dataframe : {file}."
         )
 
-        for column in column_exceptions:
-            if column.column_name not in dataframe.columns:
-                raise column.error(file=file, column=column.column_name)  # type: ignore[arg-type]
+        for column_ in column_exceptions:
+            if column_.column_name not in dataframe.columns:
+                raise column_.error(file=file, column=column_.column_name)  # type: ignore[arg-type]
 
     @staticmethod
     def convert_dtype(
-        dataframe: pd.DataFrame, dtype_dict: dict[str, str], time_column: str
+        dataframe: pd.DataFrame,
+        dtype_dict: dict[str, str],
+        time_column: str,
+        file: Path,
     ) -> pd.DataFrame:
         """
         Méthode permettant de convertir et nettoyer le dataframe.
@@ -58,17 +61,21 @@ class DataParserABC(ABC):
         :param dataframe: (pd.DataFrame) Le dataframe à convertir.
         :param dtype_dict: (dict[str, str]) Un dictionnaire de type de données.
         :param time_column: (str) Le nom de la colonne de temps.
+        :param file: (Path) Le fichier source.
         :return: (pd.DataFrame) Le dataframe converti et nettoyé.
         """
-        LOGGER.debug("Conversion du dtype des colonnes et nettoyage du dataframe.")
+        LOGGER.debug(
+            f"Conversion du dtype des colonnes {[column_ for column_ in dtype_dict.keys()] + [time_column]}"
+            f" et nettoyage du dataframe : {file}."
+        )
 
         with WarningCapture() as warnings_list:
             dataframe[time_column] = pd.to_datetime(
                 dataframe[time_column], errors="coerce"
             )
 
-            for column in dtype_dict.keys():
-                dataframe[column] = pd.to_numeric(dataframe[column], errors="coerce")
+            for column_ in dtype_dict.keys():
+                dataframe[column_] = pd.to_numeric(dataframe[column_], errors="coerce")
 
         if warnings_list.captured_warnings:
             LOGGER.warning(
@@ -79,9 +86,8 @@ class DataParserABC(ABC):
 
         return dataframe
 
-    @staticmethod
     @abstractmethod
-    def read(file: Path, **kwargs) -> gpd.GeoDataFrame:
+    def read(self, file: Path, **kwargs) -> gpd.GeoDataFrame:
         """
         Méthode permettant de lire un fichier brut et retourne un geodataframe.
 
@@ -90,7 +96,6 @@ class DataParserABC(ABC):
         """
         pass
 
-    @abstractmethod
     def read_files(self, files: Collection[Path]) -> gpd.GeoDataFrame:
         """
         Méthode permettant de lire les fichiers brutes et retourne un geodataframe.
@@ -98,6 +103,10 @@ class DataParserABC(ABC):
         :param files: (Collection[Path]) Les fichiers à lire.
         :return: (gpd.GeoDataFrame) Un GeoDataFrame.
         """
+        LOGGER.debug(
+            f"Conversion des fichiers de données brutes en geodataframe : {files}"
+        )
+
         geodataframe_list = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = [executor.submit(self.read, file) for file in files]
@@ -116,6 +125,37 @@ class DataParserABC(ABC):
         """
         pass
 
+    @staticmethod
+    def remove_duplicates(data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        """
+        Méthode permettant de supprimer les doublons du geodataframe.
+
+        :param data: (gpd.GeoDataFrame) Le geodataframe à nettoyer.
+        :return: (gpd.GeoDataFrame) Le geodataframe nettoyé.
+        """
+        LOGGER.debug("Suppression des doublons.")
+
+        data = data.drop_duplicates(
+            subset=[TIME_UTC, LATITUDE_WGS84, LONGITUDE_WGS84, DEPTH_METER]
+        )
+
+        return data
+
+    @staticmethod
+    def sort_geodataframe_by_datetime(data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        """
+        Méthode permettant de trier le geodataframe par datetime.
+
+        :param data: (gpd.GeoDataFrame) Le geodataframe à trier.
+        :return: (gpd.GeoDataFrame) Le geodataframe trié.
+        """
+        LOGGER.debug("Tri du geodataframe par datetime.")
+
+        data = data.reset_index(drop=True)
+        data = data.sort_values(by=[TIME_UTC])
+
+        return data
+
     @classmethod
     def from_files(cls, files: Collection[Path]) -> gpd.GeoDataFrame:
         """
@@ -129,19 +169,7 @@ class DataParserABC(ABC):
         data_geodataframe: gpd.GeoDataFrame[DataLoggerSchema] = parser.transform(
             data=data_geodataframe
         )
-
-        LOGGER.debug("Suppression des doublons.")
-        data_geodataframe = data_geodataframe.drop_duplicates(
-            subset=[
-                TIME_UTC,
-                LATITUDE_WGS84,
-                LONGITUDE_WGS84,
-                DEPTH_METER,
-            ]
-        )
-
-        LOGGER.debug("Tri du geodataframe par datetime.")
-        data_geodataframe = data_geodataframe.reset_index(drop=True)
-        data_geodataframe = data_geodataframe.sort_values(by=[TIME_UTC])
+        data_geodataframe = parser.remove_duplicates(data=data_geodataframe)
+        data_geodataframe = parser.sort_geodataframe_by_datetime(data=data_geodataframe)
 
         return data_geodataframe
