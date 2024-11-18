8eff7e9f198d9a251e057a178f74e27cc04dd846
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-18 10:12:31 -0500

    ajout de validate_schema

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,8 +1,9 @@
-from .model import DataLoggerSchema
+from .model import DataLoggerSchema, validate_schema
 from .model_ids import TIME_UTC, LATITUDE_WGS84, LONGITUDE_WGS84, DEPTH_METER
 
 __all__ = [
     "DataLoggerSchema",
+    "validate_schema",
     "TIME_UTC",
     "LATITUDE_WGS84",
     "LONGITUDE_WGS84",
