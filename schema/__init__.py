c05de0eafea94076fae5da64d8f360e334478789
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-18 10:11:54 -0500

    import au niveau de la librairie

---
+++ schema/__init__.py
@@ -0,0 +1,10 @@
+from .model import DataLoggerSchema
+from .model_ids import TIME_UTC, LATITUDE_WGS84, LONGITUDE_WGS84, DEPTH_METER
+
+__all__ = [
+    "DataLoggerSchema",
+    "TIME_UTC",
+    "LATITUDE_WGS84",
+    "LONGITUDE_WGS84",
+    "DEPTH_METER",
+]
