317a3059b937395a671a4d741f29c6b937eeee21
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-19 15:30:18 -0500

    modification du nom des constantes et ajout des constantes actisense

--- ingestion/parser_ids.py
+++ ingestion/parser_ids.py
@@ -1,7 +1,7 @@
 # Constantes pour les noms des colonnes des données OFM et DCDB.
-LON: str = "LON"
-LAT: str = "LAT"
-DEPTH: str = "DEPTH"
+LONGITUDE_DCDB: str = "LON"
+LATITUDE_DCDB: str = "LAT"
+DEPTH_DCDB: str = "DEPTH"
 TIME: str = "TIME"
 
 # Constantes pour les noms des colonnes des données Lowrance.
@@ -9,6 +9,20 @@ LONGITUDE_LOWRANCE: str = "Longitude[°WGS84]"
 LATITUDE_LOWRANCE: str = "Latitude[°WGS84]"
 DEPTH_LOWRANCE: str = "WaterDepth[Feet]"
 TIME_LOWRANCE: str = "DateTime[UTC]"
+
+# Constantes pour les noms des colonnes des données Actisense.
+LINE_ACTISENSE: str = "Line"
+TIME_ACTISENSE: str = "Time"
+NAME_ACTISENSE: str = "Name"
+DEPTH_ACTISENSE: str = "Water Depth Transducer"
+OFFSET_ACTISENSE: str = "Offset"
+POSITION_DATE_ACTISENSE: str = "Position date"
+POSITION_TIME_ACTISENSE: str = "Position time"
+LATITUDE_ACTISENSE: str = "Latitude.1"
+LONGITUDE_ACTISENSE: str = "Longitude.1"
+COURSE_OVER_GROUND_ACTISENSE: str = "Course Over Ground"
+SPEED_OVER_GROUND_ACTISENSE: str = "Speed Over Ground"
+PGN_ACTISENSE: str = "PGN"
 
 # Constantes pour les dtypes des données.
 FLOAT64: str = "float64"
