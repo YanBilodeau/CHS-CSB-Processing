b3d43a0b2119d52f198bb85602fcb9c836bac95e
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-20 15:57:24 -0500

    commit initial

---
+++ vessel/vessel_ids.py
@@ -0,0 +1,27 @@
+# Constantes pour VesselCongig
+ID: str = "id"
+AXIS_CONVENTION: str = "axis_convention"
+NAV: str = "nav"
+MOTION: str = "motion"
+SOUNDER: str = "sounder"
+WATERLINE: str = "waterline"
+SSP_APPLIED: str = "ssp_applied"
+ATTRIBUTE: str = "attribute"
+
+# Constantes pour la convention des axes
+CARIS: str = "CARIS"
+
+# Contantes pour les senseurs
+TIME_STAMP: str = "time_stamp"
+X: str = "x"
+Y: str = "y"
+
+# Constante pour les attributs
+PLTFRM: str = "pltfrm"
+SDGHDW: str = "sdghdw"
+POSHDW: str = "poshdw"
+BUREAU: str = "bureau"
+RESTRN: str = "restrn"
+
+# Constante pour le ssp
+SSP: str = "ssp"
