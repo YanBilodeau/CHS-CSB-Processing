0cc13dbce9945f8254f0af1b9ae58ec991f37264
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-20 11:11:09 -0500

    ajout d'exemple

--- example_parser.py
+++ example_parser.py
@@ -5,11 +5,12 @@ import geopandas as gpd
 from loguru import logger
 
 from export.export_utils import export_geodataframe_to_geojson
+from ingestion import factory_parser
 from ingestion.parser_abc import DataParserABC
 from ingestion.parser_dcdb import DataParserBCDB
 from ingestion.parser_lowrance import DataParserLowrance
 import transformation.data_cleaning as cleaner
-
+from config.data_config import get_data_config, DataFilterConfig
 
 LOGGER = logger.bind(name="Example.Parser")
 
@@ -45,17 +46,36 @@ def get_lowrance_files() -> tuple[list[Path], Type[DataParserABC]]:
     ], DataParserLowrance
 
 
+def get_blackbox_files() -> tuple[list[Path], Type[DataParserABC]]:
+    return [ROOT / "ingestion" / "BlackBox" / "NMEALOG.TXT"], None
+
+
+def get_actisense_files() -> tuple[list[Path], Type[DataParserABC]]:
+    return [
+        ROOT / "ingestion" / "ActiSense" / "composite_RDL_2024_ALL.n2kdecoded.csv"
+    ], None
+
+
 def main() -> None:
     if not EXPORT.exists():
         EXPORT.mkdir()
+
+    data_config: DataFilterConfig = get_data_config()
 
     LOGGER.info("Parser Test")
     files, parser = get_ofm_files()
     # files, parser = get_dcdb_files()
     # files, parser = get_lowrance_files()
+    # files, parser = get_blackbox_files()
+    # files, parser = get_actisense_files()
+
+    for file in files:
+        LOGGER.info(f"File: {file}")
+        header = factory_parser.get_parser_factory(file)
+        print(header)
 
     data: gpd.GeoDataFrame = parser.from_files(files=files)
-    data = cleaner.clean_data(data)
+    data = cleaner.clean_data(data, data_filter=data_config)
 
     export_geodataframe_to_geojson(data, EXPORT / "ParsedData.geojson")
 
