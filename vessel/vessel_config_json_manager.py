24d9a15bb3a90a2fdbda6e9616f6e8b2da904480
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-21 09:48:55 -0500

    ajout d'info dans le log

--- vessel/vessel_config_json_manager.py
+++ vessel/vessel_config_json_manager.py
@@ -1,22 +1,28 @@
 import json
 from pathlib import Path
 
+from loguru import logger
+
 from .vessel_config_manager_abc import VesselConfigManagerABC
 from .vessel_config import (
     VesselConfig,
     get_vessel_config_from_config_dict,
     VesselConfigDict,
 )
+from . import vessel_ids as ids
+
+LOGGER = logger.bind(name="CSB-Pipeline.VesselConfigManager.JSON")
 
 
 class VesselConfigJsonManager(VesselConfigManagerABC):
     def __init__(self, json_config_path: Path):
-        self._vessel_configs = self._load_vessel_configs(
+        super().__init__()
+        self._vessel_configs = self._load_vessel_configs_file(
             json_config_path=json_config_path
         )
 
     @staticmethod
-    def _load_vessel_configs(json_config_path: Path) -> dict[str, VesselConfig]:
+    def _load_vessel_configs_file(json_config_path: Path) -> dict[str, VesselConfig]:
         """
         Méthode permettant de charger la configuration des navires depuis un fichier JSON.
 
@@ -24,6 +30,10 @@ class VesselConfigJsonManager(VesselConfigManagerABC):
         :return: (dict[str, VesselConfigDict]) Les configurations des navires.
         :raises FileNotFoundError: Si le fichier de configuration des navires n'existe pas.
         """
+        LOGGER.debug(
+            f"Chargement du fichier de configuration des navires : {json_config_path}."
+        )
+
         if not json_config_path.exists():
             raise FileNotFoundError(
                 f"Le fichier de configuration des navires n'existe pas: {json_config_path}."
@@ -33,7 +43,7 @@ class VesselConfigJsonManager(VesselConfigManagerABC):
             vessel_configs: list[VesselConfigDict] = json.load(file)
 
         return {
-            vessel["id"]: get_vessel_config_from_config_dict(vessel)
+            vessel[ids.ID]: get_vessel_config_from_config_dict(vessel)
             for vessel in vessel_configs
         }
 
@@ -45,6 +55,8 @@ class VesselConfigJsonManager(VesselConfigManagerABC):
         :return: (VesselConfig) Configuration du navire.
         :raises ValueError: Si la configuration du navire n'existe pas.
         """
+        LOGGER.debug(f"Récupération de la configuration du navire : {vessel_id}.")
+
         if vessel_id not in self._vessel_configs:
             raise ValueError(f"La configuration du navire {vessel_id} n'existe pas.")
 
@@ -56,4 +68,6 @@ class VesselConfigJsonManager(VesselConfigManagerABC):
 
         :return: (list[VesselConfig]) Configurations des navires.
         """
+        LOGGER.debug("Récupération de la configuration de tous les navires.")
+
         return [config for config in self._vessel_configs.values()]
