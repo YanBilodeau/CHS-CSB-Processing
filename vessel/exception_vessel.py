a23afa09676d5d82b2b77e55b0cf571ddd25f24c
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-21 12:57:18 -0500

    ajout de SensorNotFoundError

--- vessel/exception_vessel.py
+++ vessel/exception_vessel.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
-@dataclass
+@dataclass(frozen=True)
 class MissingConfigKeyError(Exception):
     missing_keys: list[str]
 
     def __str__(self) -> str:
         return f"Certaines clés de configuration sont manquantes: {self.missing_keys}."
+
+
+@dataclass(frozen=True)  #
+class SensorNotFoundError(Exception):
+    sensor_name: str
+    timestamp: datetime
+
+    def __str__(self) -> str:
+        return f"Aucun capteur de type {self.sensor_name} trouvé pour le timestamp {self.timestamp}."
