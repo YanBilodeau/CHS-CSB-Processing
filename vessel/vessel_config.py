9846334e50d6d6f9ed7563bf0e47e69f27bff274
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-21 12:59:30 -0500

    ajout de getter selon le timestamp pour tous les sensors

--- vessel/vessel_config.py
+++ vessel/vessel_config.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from enum import StrEnum
 
 from pydantic import BaseModel
 
-from .exception_vessel import MissingConfigKeyError
+from .exception_vessel import MissingConfigKeyError, SensorNotFoundError
 from . import vessel_ids as ids
 from .vessel_models import VesselConfigDict
 
@@ -57,6 +57,91 @@ class VesselConfig(BaseModel):
     waterline: list[Waterline]
     ssp_applied: list[SoundSpeedProfile]
     attribute: list[BDBattribute]
+
+    def get_sensor(
+        self, sensor_name: str, timestamp: datetime
+    ) -> Sensor | Waterline | SoundSpeedProfile | BDBattribute:
+        """
+        Récupère les données d'un type de capteur à un instant donné.
+
+        :param sensor_name: (str) Nom du capteur.
+        :param timestamp: (datetime) Une date et heure.
+        :return: (Sensor | Waterline | SoundSpeedProfile | BDBattribute) Données du capteur pour le moment donné.
+        """
+        sensors: list[Sensor | Waterline | SoundSpeedProfile | BDBattribute] = getattr(
+            self, sensor_name
+        )
+        closest_sensor: Sensor | Waterline | SoundSpeedProfile | BDBattribute | None = (
+            None
+        )
+        closest_time_diff: timedelta | None = None
+
+        for sensor in sensors:
+            time_diff: timedelta = timestamp - sensor.time_stamp
+            if time_diff.total_seconds() >= 0 and (
+                closest_time_diff is None or time_diff < closest_time_diff
+            ):
+                closest_sensor = sensor
+                closest_time_diff = time_diff
+
+        if closest_sensor is None:
+            raise SensorNotFoundError(sensor_name=sensor_name, timestamp=timestamp)
+
+        return closest_sensor
+
+    def get_navigation(self, timestamp: datetime) -> Sensor:
+        """
+        Méthode pour récupérer les données de navigation à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (Sensor) Données de navigation pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.NAVIGATION, timestamp=timestamp)
+
+    def get_motion(self, timestamp: datetime) -> Sensor:
+        """
+        Méthode pour récupérer les données de mouvement à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (Sensor) Données de mouvement pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.MOTION, timestamp=timestamp)
+
+    def get_sounder(self, timestamp: datetime) -> Sensor:
+        """
+        Méthode pour récupérer les données du sondeur à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (Sensor) Données du sondeur pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.SOUNDER, timestamp=timestamp)
+
+    def get_waterline(self, timestamp: datetime) -> Waterline:
+        """
+        Méthode pour récupérer les données de ligne d'eau à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (Waterline) Données de ligne d'eau pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.WATERLINE, timestamp=timestamp)
+
+    def get_ssp_applied(self, timestamp: datetime) -> SoundSpeedProfile:
+        """
+        Méthode pour récupérer les données de profil de vitesse du son appliqué à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (SoundSpeedProfile) Données de profil de vitesse du son appliqué pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.SSP_APPLIED, timestamp=timestamp)
+
+    def get_attribute(self, timestamp: datetime) -> BDBattribute:
+        """
+        Méthode pour récupérer les données d'attribut à un instant donné.
+
+        :param timestamp: (datetime) Une date et heure.
+        :return: (BDBattribute) Données d'attribut pour le moment donné.
+        """
+        return self.get_sensor(sensor_name=ids.ATTRIBUTE, timestamp=timestamp)
 
 
 def get_vessel_config_from_config_dict(config: VesselConfigDict) -> VesselConfig:
