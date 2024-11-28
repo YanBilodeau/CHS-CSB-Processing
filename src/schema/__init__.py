9480239083cc05175e4b3aab7f949c82e4d84847
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-11-28 13:15:49 -0500

    ajout des schema de tide

--- src/schema/__init__.py
+++ src/schema/__init__.py
@@ -2,11 +2,20 @@
 Ce package contient les schémas des données.
 """
 
-from .model import DataLoggerSchema, validate_schema
+from .model import (
+    DataLoggerSchema,
+    StationsSchema,
+    TideZoneSchema,
+    TimeSerieDataSchema,
+    validate_schema,
+)
 from .model_ids import TIME_UTC, LATITUDE_WGS84, LONGITUDE_WGS84, DEPTH_METER
 
 __all__ = [
     "DataLoggerSchema",
+    "StationsSchema",
+    "TideZoneSchema",
+    "TimeSerieDataSchema",
     "validate_schema",
     "TIME_UTC",
     "LATITUDE_WGS84",
