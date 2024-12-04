313e6f1650f2a1c34c455b1f675d12f128f521d5
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-04 15:39:17 -0500

    modification du format du log

--- src/logger/ids_logger.py
+++ src/logger/ids_logger.py
@@ -1,3 +1,3 @@
 LOG_FORMAT: str = (
-    "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: ^8} | {process}:{name}:{module}:{function}:{line}:{extra[hostname]}:{extra[username]} - {message}"
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: ^8} | {process}:{thread}:{name}:{module}:{function}:{line}:{extra[hostname]}:{extra[username]} - {message}"
 )
