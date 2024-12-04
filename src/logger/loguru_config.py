fbb7eed58e4e2d0817255a8abe92cd3571342dc1
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-04 15:37:47 -0500

    modification du format du log

--- src/logger/loguru_config.py
+++ src/logger/loguru_config.py
@@ -41,7 +41,7 @@ def formatter(record) -> str:
     return (
         "<bold><white>"
         f"<{color_tag}>"
-        "{extra[name]: <35}"
+        "{extra[name]: <40}"
         f"</{color_tag}> | "
         "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | </white></bold> "
         "<level>{level: ^8}</level> <bold><white>-</white></bold> <level>{message}</level>\n"
