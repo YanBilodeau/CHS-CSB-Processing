aca586a521063ac1b00fcd80a4a87a653dcd7d28
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-04 13:53:31 -0500

    ajustement des imports et du __all__

--- src/tide/time_serie/__init__.py
+++ src/tide/time_serie/__init__.py
@@ -6,22 +6,14 @@ from .exception_time_serie import (
     WaterLevelDataGapError,
     NoWaterLevelDataError,
     InterpolationValueError,
-    get_data_gaps_message,
 )
 from .time_serie_dataframe import (
-    get_time_series_data,
-    process_gaps_to_fill,
-    get_water_level_data,
-    identify_data_gaps,
+    get_water_level_data_for_stations,
 )
 
 
 __all__ = [
-    "get_time_series_data",
-    "process_gaps_to_fill",
-    "get_water_level_data",
-    "identify_data_gaps",
-    "get_data_gaps_message",
+    "get_water_level_data_for_stations",
     "WaterLevelDataGapError",
     "NoWaterLevelDataError",
     "InterpolationValueError",
