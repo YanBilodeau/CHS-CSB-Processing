2a64116f9ef3ab1483acfad790bf5dff1fa43322
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-05 10:17:31 -0500

    ajout d'export pour shp, csv et GPKG

--- src/export/export_utils.py
+++ src/export/export_utils.py
@@ -34,6 +34,31 @@ def transform_geodataframe_crs(geodataframe: gpd.GeoDataFrame, to_epsg: int) -> 
         geodataframe.to_crs(epsg=to_epsg, inplace=True)
 
 
+def export_geodataframe(
+    geodataframe: gpd.GeoDataFrame,
+    driver: str,
+    output_path: Path,
+    to_epsg: Optional[int] = WGS84,
+    **kwargs,
+) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier GeoJSON.
+
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    :param driver: Le driver du fichier de sortie.
+    :type driver: str
+    :param to_epsg: Le code EPSG de la projection.
+    :type to_epsg: Optional[int]
+    """
+    LOGGER.debug(f"Sauvegarde du GeoDataFrame en fichier {driver} : '{output_path}'.")
+
+    transform_geodataframe_crs(geodataframe=geodataframe, to_epsg=to_epsg)
+    geodataframe.to_file(str(sanitize_path_name(output_path)), driver=driver, **kwargs)
+
+
 def export_geodataframe_to_geojson(
     geodataframe: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
 ) -> None:
@@ -47,12 +72,57 @@ def export_geodataframe_to_geojson(
     :param to_epsg: Le code EPSG de la projection.
     :type to_epsg: Optional[int]
     """
-    LOGGER.debug(f"Sauvegarde du GeoDataFrame en fichier GeoJSON : '{output_path}'.")
-
-    transform_geodataframe_crs(geodataframe=geodataframe, to_epsg=to_epsg)
-    geodataframe.to_file(
-        str(sanitize_path_name(output_path)), driver="GeoJSON"
+    export_geodataframe(
+        geodataframe, "GeoJSON", output_path, to_epsg=to_epsg
     )  # , RFC7946=True)
+
+
+def export_geodataframe_to_shapefile(
+    geodataframe: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
+) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier Shapefile.
+
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    :param to_epsg: Le code EPSG de la projection.
+    :type to_epsg: Optional[int]
+    """
+    export_geodataframe(geodataframe, "ESRI Shapefile", output_path, to_epsg=to_epsg)
+
+
+def export_geodataframe_to_gpkg(
+    gdf: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
+) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier GeoPackage.
+
+    :param gdf: Le GeoDataFrame.
+    :type gdf: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    :param to_epsg: Le code EPSG de la projection.
+    :type to_epsg: Optional[int]
+    """
+    export_geodataframe(gdf, "GPKG", output_path, to_epsg=to_epsg)
+
+
+def export_geodataframe_to_csv(
+    gdf: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
+) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier CSV.
+
+    :param gdf: Le GeoDataFrame.
+    :type gdf: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    :param to_epsg: Le code EPSG de la projection.
+    :type to_epsg: Optional[int]
+    """
+    export_geodataframe(gdf, "CSV", output_path, to_epsg=to_epsg)
 
 
 def export_dataframe_to_csv(dataframe: pd.DataFrame, output_path: Path) -> None:
