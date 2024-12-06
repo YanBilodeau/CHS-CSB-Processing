5301f116fb3848d3ce7cc193f1437edc16e706c9
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-06 10:12:11 -0500

    formatage

--- src/vessel/__init__.py
+++ src/vessel/__init__.py
@@ -2,8 +2,23 @@
 Ce package contient les classes et les fonctions qui permettent de manipuler les données des navires.
 """
 
-from .vessel_config import VesselConfig, AxisConvention, Sensor, BDBattribute, Waterline, SoundSpeedProfile
+from .vessel_config import (
+    VesselConfig,
+    AxisConvention,
+    Sensor,
+    BDBattribute,
+    Waterline,
+    SoundSpeedProfile,
+)
 from .vessel_config_json_manager import VesselConfigJsonManager
 
 
-__all__ = ['VesselConfig', 'AxisConvention', 'Sensor', 'BDBattribute', 'Waterline', 'SoundSpeedProfile', 'VesselConfigJsonManager']
+__all__ = [
+    "VesselConfig",
+    "AxisConvention",
+    "Sensor",
+    "BDBattribute",
+    "Waterline",
+    "SoundSpeedProfile",
+    "VesselConfigJsonManager",
+]
