27e6e54f31ff01889870b804b279feaf0d1a73fe
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-06 08:52:52 -0500

    ajout de SensorProtocol et WaterlineProtocol

--- src/transformation/transformation_models.py
+++ src/transformation/transformation_models.py
@@ -4,6 +4,7 @@ Module pour les modèles de transformation.
 Ce module contient les modèles de données pour la transformation des données.
 """
 
+from datetime import datetime
 from typing import Protocol, Optional
 
 
@@ -31,3 +32,43 @@ class DataFilterConfigProtocol(Protocol):
     """La profondeur minimale."""
     max_depth: Optional[int | float]
     """La profondeur maximale."""
+
+
+class SensorProtocol(Protocol):
+    """
+    Protocole pour les données pour un capteur.
+
+    :param time_stamp: Date et heure.
+    :type time_stamp: datetime
+    :param x: Bras de levier X.
+    :type x: float
+    :param y: Bras de levier Y.
+    :type y: float
+    :param z: Bras de levier Z.
+    :type z: float
+    """
+
+    time_stamp: datetime
+    """Date et heure."""
+    x: float
+    """Bras de levier X."""
+    y: float
+    """Bras de levier Y."""
+    z: float
+    """Bras de levier Z."""
+
+
+class WaterlineProtocol(Protocol):
+    """
+    Protocole pour les données pour une ligne d'eau.
+
+    :param time_stamp: Date et heure.
+    :type time_stamp: datetime
+    :param z: Bras de levier Z.
+    :type z: float
+    """
+
+    time_stamp: datetime
+    """Date et heure."""
+    z: float
+    """Bras de levier Z."""
