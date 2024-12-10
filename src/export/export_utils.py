626e3c6982ef7532917994509c0829fb8ccd1c8e
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-10 11:06:35 -0500

    ajout de export_geodataframe_to_parquet et export_geodataframe_to_feather

--- src/export/export_utils.py
+++ src/export/export_utils.py
@@ -94,42 +94,70 @@ def export_geodataframe_to_shapefile(
 
 
 def export_geodataframe_to_gpkg(
-    gdf: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
+    geodataframe: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
 ) -> None:
     """
     Sauvegarde le GeoDataFrame dans un fichier GeoPackage.
 
-    :param gdf: Le GeoDataFrame.
-    :type gdf: gpd.GeoDataFrame
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
     :param output_path: Le chemin du fichier de sortie.
     :type output_path: Path
     :param to_epsg: Le code EPSG de la projection.
     :type to_epsg: Optional[int]
     """
-    export_geodataframe(gdf, "GPKG", output_path, to_epsg=to_epsg)
+    export_geodataframe(geodataframe, "GPKG", output_path, to_epsg=to_epsg)
 
 
 def export_geodataframe_to_csv(
-    gdf: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
+    geodataframe: gpd.GeoDataFrame, output_path: Path, to_epsg: Optional[int] = WGS84
 ) -> None:
     """
     Sauvegarde le GeoDataFrame dans un fichier CSV.
 
-    :param gdf: Le GeoDataFrame.
-    :type gdf: gpd.GeoDataFrame
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
     :param output_path: Le chemin du fichier de sortie.
     :type output_path: Path
     :param to_epsg: Le code EPSG de la projection.
     :type to_epsg: Optional[int]
     """
     # Transformer le système de coordonnées si nécessaire
-    transform_geodataframe_crs(gdf, to_epsg)
+    transform_geodataframe_crs(geodataframe, to_epsg)
 
     # Convertir le GeoDataFrame en DataFrame en supprimant la colonne de géométrie
-    df = gdf.drop(columns=gdf.geometry.name)
+    df = geodataframe.drop(columns=geodataframe.geometry.name)
 
     # Sauvegarder le DataFrame en CSV
     export_dataframe_to_csv(df, output_path)
+
+
+def export_geodataframe_to_parquet(geodataframe: gpd.GeoDataFrame, output_path: Path) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier Parquet.
+
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    """
+    LOGGER.debug(f"Sauvegarde du GeoDataFrame en fichier Parquet : '{output_path}'.")
+
+    geodataframe.to_parquet(sanitize_path_name(output_path), index=False)
+
+
+def export_geodataframe_to_feather(geodataframe: gpd.GeoDataFrame, output_path: Path) -> None:
+    """
+    Sauvegarde le GeoDataFrame dans un fichier Feather.
+
+    :param geodataframe: Le GeoDataFrame.
+    :type geodataframe: gpd.GeoDataFrame
+    :param output_path: Le chemin du fichier de sortie.
+    :type output_path: Path
+    """
+    LOGGER.debug(f"Sauvegarde du GeoDataFrame en fichier Feather : '{output_path}'.")
+
+    geodataframe.to_feather(sanitize_path_name(output_path))
 
 
 def export_dataframe_to_csv(dataframe: pd.DataFrame, output_path: Path) -> None:
