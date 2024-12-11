e908aef65e68e06b137bda94cd85ad55eb0ca0f9
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-11 11:52:31 -0500

    ajout de init_cache

--- src/tide/stations/cache_wrapper.py
+++ src/tide/stations/cache_wrapper.py
@@ -13,10 +13,24 @@ from loguru import logger
 
 
 LOGGER = logger.bind(name="CSB-Pipeline.Tide.Station.Cache")
-CACHE: Cache = Cache(str(Path(__file__).parent / "cache"))
+cache: Cache | None = None
 
 
-def cache_result(ttl: int = 86400):
+def init_cache(cache_path: Path) -> None:
+    """
+    Fonction pour initialiser le cache.
+
+    :param cache_path: Chemin du cache.
+    :type cache_path: Path
+    """
+    global cache
+
+    if cache is None:
+        LOGGER.debug(f"Initialisation du cache avec le chemin : {cache_path}.")
+        cache = Cache(str(cache_path))
+
+
+def cache_result(ttl: int = 86400) -> Callable:
     """
     Décorateur pour mettre en cache le résultat d'une fonction.
 
@@ -45,16 +59,16 @@ def cache_result(ttl: int = 86400):
             """
             cache_key = f"{func.__name__}_{args}_{kwargs}"
 
-            if cache_key in CACHE:
-                LOGGER.trace(f"Récupération des données depuis la cache : {cache_key}.")
-                return CACHE[cache_key]
+            if cache_key in cache:
+                LOGGER.trace(f"Récupération des données depuis le cache : {cache_key}.")
+                return cache[cache_key]
 
             result = func(*args, **kwargs)
 
             LOGGER.trace(
-                f"Ajout de données dans la cache avec un ttl de {ttl} secondes : '{cache_key}'."
+                f"Ajout de données dans le cache avec un ttl de {ttl} secondes : '{cache_key}'."
             )
-            CACHE.set(key=cache_key, value=result, expire=ttl)
+            cache.set(key=cache_key, value=result, expire=ttl)
 
             return result
 
@@ -67,5 +81,5 @@ def clear_cache():
     """
     Fonction pour vider le cache.
     """
-    CACHE.clear()
+    cache.clear()
     LOGGER.debug("Le cache a été vidé.")
