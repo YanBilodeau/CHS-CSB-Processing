1387d0a8021898d4a34754f8eae87494f5174cd1
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-11 14:47:50 -0500

    ajout des fonctions get

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -7,8 +7,9 @@ from .data_config import (
     DataGeoreferenceConfig,
     VesselManagerConfig,
     CSBprocessingConfig,
+    get_data_config,
 )
-from .iwls_api_config import IWLSAPIConfig
+from .iwls_api_config import IWLSAPIConfig, get_api_config
 
 __all__ = [
     "DataFilterConfig",
@@ -16,4 +17,6 @@ __all__ = [
     "VesselManagerConfig",
     "IWLSAPIConfig",
     "CSBprocessingConfig",
+    "get_data_config",
+    "get_api_config",
 ]
