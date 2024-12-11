dfb3025177a80e1068c72a780d6ef538ed64f25c
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2024-12-11 14:43:20 -0500

    ajout d'import

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,3 +1,14 @@
 """
 Ce package contient les configurations nécessaires pour l'application.
 """
+
+from .data_config import DataFilterConfig, DataGeoreferenceConfig, VesselManagerConfig, CSBprocessingConfig
+from .iwls_api_config import IWLSAPIConfig
+
+__all__ = [
+    "DataFilterConfig",
+    "DataGeoreferenceConfig",
+    "VesselManagerConfig",
+    "IWLSAPIConfig",
+    "CSBprocessingConfig",
+    ]
