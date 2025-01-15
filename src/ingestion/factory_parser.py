e5f374a8332dda7f6b754243b8787ba10891ebf1
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-01-15 08:24:45 -0500

    utilisation du module parser_models

--- src/ingestion/factory_parser.py
+++ src/ingestion/factory_parser.py
@@ -4,8 +4,6 @@ Module permettant de récupérer le parser associé à un fichier.
 Ce module contient les fonctions permettant de récupérer le parser associé à un fichier.
 """
 
-from dataclasses import dataclass, field
-from enum import StrEnum
 from pathlib import Path
 from typing import Type, Collection
 
@@ -16,7 +14,8 @@ from . import parser_ids as ids
 from .parser_dcdb import DataParserBCDB
 from .parser_lowrance import DataParserLowrance
 from .parser_ofm import DataParserOFM
-from .parser_exception import ParserIdentifierError, MultipleParsersError, DataParserABC
+from .parser_exception import ParserIdentifierError, DataParserABC
+from .parser_models import ParserFiles
 
 LOGGER = logger.bind(name="CSB-Processing.Ingestion.Parser.Factory")
 
@@ -154,47 +153,6 @@ def get_parser_factory(file: Path) -> Type[DataParserABC]:
             return parser
 
     raise ParserIdentifierError(file=file)
-
-
-class DataType(StrEnum):
-    """
-    Enumération des types de données.
-    """
-
-    OFM: str = ids.OFM
-    """Type de données OFM."""
-    DCDB: str = ids.DCDB
-    """Type de données DCDB."""
-    LOWRANCE: str = ids.LOWRANCE
-    """Type de données Lowrance."""
-    ACTISENSE: str = ids.ACTISENSE
-    """Type de données Actisense."""
-    BLACKBOX: str = ids.BLACKBOX
-    """Type de données BlackBox."""
-
-
-@dataclass
-class ParserFiles:
-    """
-    Classe permettant de stocker les fichiers et le parser associés.
-
-    :param parser: Le parser associé aux fichiers.
-    :type parser: Type[DataParserABC]
-    :param files: Les fichiers à traiter.
-    :type files: list[Path]
-    """
-
-    parser: Type[DataParserABC] = None
-    """Le parser associé aux fichiers."""
-    files: list[Path] = field(default_factory=list)
-    """Les fichiers à traiter."""
-    data_file_type = None
-
-    def __setattr__(self, name, value):
-        if name == "parser" and self.parser is not None and self.parser != value:
-            raise MultipleParsersError(parsers=(self.parser, value))
-
-        super().__setattr__(name, value)
 
 
 def get_files_parser(files: Collection[Path,]) -> ParserFiles:
