166846feaf3457a1c6372ea8ca841ba3695821ae
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-01-29 11:29:44 -0500

    commit initial

---
+++ src/caris_api/model_caris.py
@@ -0,0 +1,14 @@
+"""
+Module définissant les modèles de données pour Caris.
+"""
+
+
+from pathlib import Path
+from typing import Protocol
+
+class CarisConfigProtocol(Protocol):
+    base_path: str
+    software: str
+    version: str
+    python_version: str
+    python_path: Path
