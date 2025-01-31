880631079e085e52ce51d1ff3434f9364bbde763
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-01-31 16:15:30 -0500

    ajout d'import

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -2,3 +2,19 @@
 Ce package contient les fonctions qui permettent de récupérer les données depuis des fichiers de
 différents formats.
 """
+
+from .parser_models import (
+    DataLoggerType,
+    DataParserABC,
+    DataParserOFM,
+    DataParserBCDB,
+    DataParserLowrance,
+)
+
+__all__ = [
+    "DataLoggerType",
+    "DataParserABC",
+    "DataParserOFM",
+    "DataParserBCDB",
+    "DataParserLowrance",
+]
