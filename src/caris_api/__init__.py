dd8c580106cfc3c94a2f409ae91eeb666bde44a8
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-01-31 13:10:21 -0500

    ajout import

--- src/caris_api/__init__.py
+++ src/caris_api/__init__.py
@@ -4,6 +4,7 @@ Ce package contient des modules pour interagir avec l'API de Caris
 
 from .pyapi.import_caris_module import CarisModuleImporter
 from .pyapi import export_csar_api
+from .caris_batch import export_csar_batch
 
 
 __all__ = ["CarisModuleImporter", "export_csar_api"]
