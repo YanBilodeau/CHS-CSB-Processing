58a9842d70696f34c89463ebb876f2f19442af28
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-03 11:42:47 -0500

    renommer attribut et ajout valeur par défaut

--- src/vessel/unknown_vessel_config.py
+++ src/vessel/unknown_vessel_config.py
@@ -13,12 +13,13 @@ from .vessel_config import (
 UNKNOWN_DATE: datetime = datetime(1960, 1, 1, tzinfo=timezone.utc)
 UNKNOWN_VESSEL_CONFIG: VesselConfig = VesselConfig(
     id="unknown",
+    name="unknown",
     axis_convention=AxisConvention.CARIS,
     navigation=[Sensor(time_stamp=UNKNOWN_DATE, x=0.0, y=0.0, z=0.0)],
     motion=[Sensor(time_stamp=UNKNOWN_DATE, x=0.0, y=0.0, z=0.0)],
     sounder=[Sensor(time_stamp=UNKNOWN_DATE, x=0.0, y=0.0, z=0.0)],
     waterline=[Waterline(time_stamp=UNKNOWN_DATE, z=0.0)],
-    ssp_applied=[SoundSpeedProfile(time_stamp=UNKNOWN_DATE, ssp=False)],
+    sound_speed=[SoundSpeedProfile(time_stamp=UNKNOWN_DATE, ssp=False, sound_speed=1500.0)],
     attribute=[
         BDBattribute(
             time_stamp=UNKNOWN_DATE,
