715aa3a5c484614ce0717512351d64857d18ac80
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-05 14:04:45 -0500

    intenum utiliser et ajout de name en tant que property

--- src/transformation/order/abc_order.py
+++ src/transformation/order/abc_order.py
@@ -6,29 +6,41 @@ Ce module contient les classes abstraites pour les ordres de transformation.
 
 from abc import ABC
 from dataclasses import dataclass
-from enum import StrEnum
+from enum import IntEnum
+from typing import Optional
 
 
-class OrderType(StrEnum):
-    exclusive_order = "Exclusive Order"
-    special_order = "Special Order"
-    order_1a = "Order 1a"
-    order_1b = "Order 1b"
-    order_2 = "Order 2"
-    order_not_met = "Order not met"
+class OrderType(IntEnum):
+    exclusive_order = 0
+    special_order = 1
+    order_1a = 2
+    order_1b = 3
+    order_2 = 4
+    order_not_met = 5
+
+    @property
+    def name(self) -> str:
+        return {
+            OrderType.exclusive_order: "Exclusive Order",
+            OrderType.special_order: "Special Order",
+            OrderType.order_1a: "Order 1a",
+            OrderType.order_1b: "Order 1b",
+            OrderType.order_2: "Order 2",
+            OrderType.order_not_met: "Order not met",
+        }[self]
 
 
 @dataclass
 class TVUorder(ABC):
-    a: float
+    a: Optional[float] = None
     """Représente la partie de l'incertitude qui ne varie pas avec la profondeur."""
-    b: float
+    b: Optional[float] = None
     """Un coefficient qui représente la partie de l'incertitude qui varie avec la profondeur."""
 
 
 @dataclass
 class THUorder(ABC):
-    constant: float
+    constant: Optional[float] = None
     """Représente la partie de l'incertitude qui ne varie pas avec la profondeur."""
-    coefficient_depth: float
+    coefficient_depth: Optional[float] = None
     """Un coefficient qui représente la partie de l'incertitude qui varie avec la profondeur."""
