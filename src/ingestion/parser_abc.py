bcd409d41b00310b98546e9f551725d66769bb76
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-05 14:19:24 -0500

    ajout de IHO Order

--- src/ingestion/parser_abc.py
+++ src/ingestion/parser_abc.py
@@ -204,6 +204,7 @@ class DataParserABC(ABC):
             schema_ids.WATER_LEVEL_INFO: pd.Series(dtype="object"),
             schema_ids.UNCERTAINTY: pd.Series(dtype="float64"),
             schema_ids.THU: pd.Series(dtype="float64"),
+            schema_ids.IHO_ORDER: pd.Series(dtype="string"),
             schema_ids.WATER_LEVEL_METER: pd.Series(dtype="float64"),
             schema_ids.TIME_SERIE: pd.Series(dtype="string"),
             schema_ids.TIDE_ZONE_ID: pd.Series(dtype="string"),
