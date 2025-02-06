642ebfc1cd936088914e275b0c9eb3e367204087
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-06 09:10:59 -0500

    ajout import

--- src/metadata/__init__.py
+++ src/metadata/__init__.py
@@ -4,5 +4,12 @@ Package pour la gestion des métadonnées.
 
 from .export import export_metadata_to_json
 from .metadata_models import CSBmetadata
+from .iho_order import IHOorderQualifiquation, OrderStatistics, classify_iho_order
 
-__all__ = ["CSBmetadata", "export_metadata_to_json"]
+__all__ = [
+    "CSBmetadata",
+    "export_metadata_to_json",
+    "IHOorderQualifiquation",
+    "OrderStatistics",
+    "classify_iho_order",
+]
