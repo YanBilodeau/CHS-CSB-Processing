270ae9cc34fabd65385f1d6aab803fe97ec33857
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-06 13:55:48 -0500

    ajout de méthode __dict__

--- src/metadata/metadata_models.py
+++ src/metadata/metadata_models.py
@@ -4,8 +4,7 @@ Module pour les modèles de métadonnées.
 Ce module contient les modèles de métadonnées pour les données du CSB.
 """
 
-from dataclasses import dataclass, asdict, field
-from datetime import datetime
+from dataclasses import dataclass, field
 from typing import Collection
 
 from loguru import logger
@@ -27,22 +26,22 @@ class CSBmetadata:
     Classe pour les métadonnées des données du CSB.
     """
 
-    start_date: datetime
+    start_date: str
     """Date de début"""
-    end_date: datetime
+    end_date: str
     """Date de fin"""
     vessel: str
     """Identifiant et nom du navire"""
     sounding_hardware: str
     """Matériel de sondage"""
-    soundding_technique: str
+    sounding_technique: str
     """Technique de sondage"""
     sounder_draft: float
     """Tirant d'eau du sondeur"""
     tvu: float
-    """Invertitude verticale"""
+    """Incertitude verticale"""
     thu: float
-    """Invertitude horizontale"""
+    """Incertitude horizontale"""
     sotfware_version: str = field(repr=False, metadata={"exclude": True})
     """Version du logiciel"""
     tide_stations: Collection[str] = field(repr=False, metadata={"exclude": True})
@@ -53,12 +52,12 @@ class CSBmetadata:
     """Méthode de positionnement"""
     resolution: str = "Point Cloud"
     """Résolution des données"""
-    data_processing_software: str = "CHS-CSB-Processing {version}"
-    """Logiciel de traitement des données"""
     horizontal_coordinate_reference_system: str = "WGS 84 - EPSG:4326"
     """Système de coordonnées horizontal"""
     vertical_coordinate_reference_system: str = "Chart Datum"
     """Système de coordonnées vertical"""
+    data_processing_software: str = "CHS-CSB-Processing {version}"
+    """Logiciel de traitement des données"""
     iho_order_statistic: IHOorderQualifiquation = None
     """Statistiques des ordre IHO"""
 
@@ -76,21 +75,24 @@ class CSBmetadata:
             else NO_TIDE_STATIONS
         )
 
-    def to_dict(self) -> dict:
+    def __dict__(self) -> dict:
         """
-        Méthode pour convertir les métadonnées en dictionnaire.
-
-        :return: Dictionnaire des métadonnées.
-        :rtype: dict
+        Convertit les données en un dictionnaire.
         """
-        LOGGER.debug("Conversion des métadonnées en dictionnaire.")
-
-        result = {}
-        for key, value in asdict(self).items():
-            if not self.__dataclass_fields__[key].metadata.get("exclude", False):
-                if isinstance(value, datetime):
-                    result[key] = value.isoformat()
-                else:
-                    result[key] = value
-
-        return result
+        return {
+            "Start Date": self.start_date,
+            "End Date": self.end_date,
+            "Vessel": self.vessel,
+            "Horizontal Coordinate Reference System": self.horizontal_coordinate_reference_system,
+            "Vertical Coordinate Reference System": self.vertical_coordinate_reference_system,
+            "Sounding Hardware": self.sounding_hardware,
+            "Sounding Technique": self.sounding_technique,
+            "Positioning Method": self.positioning_method,
+            "Sounder Draft (m)": self.sounder_draft,
+            "TVU (m)": self.tvu,
+            "THU (m)": self.thu,
+            "Water Level Reduction Method": self.water_Level_reduction_method,
+            "Resolution": self.resolution,
+            "Data Processing Software": self.data_processing_software,
+            "IHO Order Statistic": self.iho_order_statistic.__dict__(),
+        }
