cc9eda6ffb3f009388bd7da96ccc55e11bee5e8c
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-02-07 15:48:45 -0500

    ajout de grah pour THU et TVU

--- src/metadata/plot.py
+++ src/metadata/plot.py
@@ -117,6 +117,53 @@ def plot_iho_order_statistics(iho_order_statistic: dict) -> go.Figure:
     return fig
 
 
+def plot_statistics(
+    iho_order_statistic: dict, keys: list[str], colors: list[str], y_title: str
+) -> go.Figure:
+    """
+    Génère un graphique des statistiques par ordre IHO.
+
+    :param iho_order_statistic: Dictionnaire contenant les statistiques des ordres IHO.
+    :type iho_order_statistic: dict
+    :param keys: Liste des clés pour les données à tracer.
+    :type keys: list[str]
+    :param colors: Liste des couleurs pour les traces.
+    :type colors: list[str]
+    :param y_title: Titre de l'axe des y.
+    :type y_title: str
+    :return: Figure Plotly contenant les statistiques.
+    :rtype: go.Figure
+    """
+    orders = [
+        order
+        for order in iho_order_statistic
+        if any(iho_order_statistic[order].get(key) is not None for key in keys)
+    ]
+
+    data = {
+        key: [iho_order_statistic[order].get(key, None) for order in orders]
+        for key in keys
+    }
+
+    fig = go.Figure()
+
+    for (name, values), color in zip(data.items(), colors):
+        fig.add_trace(
+            go.Scatter(
+                x=orders,
+                y=values,
+                mode="lines+markers",
+                name=name,
+                marker=dict(size=10, color=color),
+                showlegend=True,
+            )
+        )
+
+    fig.update_layout(yaxis_title=y_title)
+
+    return fig
+
+
 def plot_depth_statistics(iho_order_statistic: dict) -> go.Figure:
     """
     Génère un graphique des statistiques de profondeur par ordre IHO.
@@ -126,43 +173,166 @@ def plot_depth_statistics(iho_order_statistic: dict) -> go.Figure:
     :return: Figure Plotly contenant les statistiques de profondeur.
     :rtype: go.Figure
     """
-    orders = [
-        order
-        for order in iho_order_statistic
-        if any(
-            iho_order_statistic[order].get(key) is not None
-            for key in ["Min Depth (m)", "Max Depth (m)", "Mean Depth (m)"]
+    return plot_statistics(
+        iho_order_statistic,
+        keys=["Min Depth (m)", "Max Depth (m)", "Mean Depth (m)"],
+        colors=[
+            "rgba(205,92,92, 0.25)",
+            "rgba(205,92,92, 0.95)",
+            "rgba(205,92,92, 0.65)",
+        ],
+        y_title="Depth (m)",
+    )
+
+
+def plot_tvu_statistics(iho_order_statistic: dict) -> go.Figure:
+    """
+    Génère un graphique des statistiques de TVU par ordre IHO.
+
+    :param iho_order_statistic: Dictionnaire contenant les statistiques des ordres IHO.
+    :type iho_order_statistic: Dict
+    :return: Figure Plotly contenant les statistiques de profondeur.
+    :rtype: go.Figure
+    """
+    return plot_statistics(
+        iho_order_statistic,
+        keys=["Min TVU (m)", "Max TVU (m)", "Mean TVU (m)"],
+        colors=[
+            "rgba(92,205,92, 0.25)",
+            "rgba(92,205,92, 0.95)",
+            "rgba(92,205,92, 0.65)",
+        ],
+        y_title="TVU (m)",
+    )
+
+
+def plot_thu_statistics(iho_order_statistic: dict) -> go.Figure:
+    """
+    Génère un graphique des statistiques de THU par ordre IHO.
+
+    :param iho_order_statistic: Dictionnaire contenant les statistiques des ordres IHO.
+    :type iho_order_statistic: Dict
+    :return: Figure Plotly contenant les statistiques de profondeur.
+    :rtype: go.Figure
+    """
+    return plot_statistics(
+        iho_order_statistic,
+        keys=["Min THU (m)", "Max THU (m)", "Mean THU (m)"],
+        colors=[
+            "rgba(92,92,205, 0.25)",
+            "rgba(92,92,205, 0.95)",
+            "rgba(92,92,205, 0.65)",
+        ],
+        y_title="THU (m)",
+    )
+
+
+def create_legend(
+    x=0.009, y=0.37, xanchor="left", yanchor="top", bgcolor="rgba(255, 255, 255, 0.7)"
+) -> dict:
+    """
+    Crée une légende pour un graphique Plotly.
+
+    :param x: Position x de la légende.
+    :type x: float
+    :param y: Position y de la légende.
+    :type y: float
+    :param xanchor: Ancrage horizontal de la légende.
+    :type xanchor: str
+    :param yanchor: Ancrage vertical de la légende.
+    :type yanchor: str
+    :param bgcolor: Couleur de fond de la légende.
+    :type bgcolor: str
+    :return: Dictionnaire de configuration de la légende.
+    :rtype: dict
+    """
+    return dict(
+        x=x,
+        y=y,
+        xanchor=xanchor,
+        yanchor=yanchor,
+        bgcolor=bgcolor,
+    )
+
+
+def create_annotations(annotations_data: list[dict]) -> list[dict]:
+    """
+    Crée une liste d'annotations pour un graphique Plotly.
+
+    :param annotations_data: Liste de dictionnaires contenant les données des annotations.
+    :type annotations_data: list[dict]
+    :return: Liste de dictionnaires de configuration des annotations.
+    :rtype: list[dict]
+    """
+    return [
+        dict(
+            text=annotation["text"],
+            x=annotation["x"],
+            y=annotation["y"],
+            xanchor=annotation["xanchor"],
+            yanchor=annotation["yanchor"],
+            font=dict(size=20, weight="bold"),
+            showarrow=False,
         )
+        for annotation in annotations_data
     ]
 
-    depth_data = {
-        "Min Depth (m)": [
-            iho_order_statistic[order].get("Min Depth (m)", None) for order in orders
-        ],
-        "Max Depth (m)": [
-            iho_order_statistic[order].get("Max Depth (m)", None) for order in orders
-        ],
-        "Mean Depth (m)": [
-            iho_order_statistic[order].get("Mean Depth (m)", None) for order in orders
-        ],
-    }
 
-    fig = go.Figure()
-    colors = ["rgba(205,92,92, 0.25)", "rgba(205,92,92, 0.95)", "rgba(205,92,92, 0.65)"]
+def update_layout(
+    fig: go.Figure, title: str, cell_color: str, annotations: list[dict], legend: dict
+) -> None:
+    """
+    Met à jour la mise en page d'une figure Plotly.
 
-    for (name, data), color in zip(depth_data.items(), colors):
-        fig.add_trace(
-            go.Scatter(
-                x=orders,
-                y=data,
-                mode="lines+markers",
-                name=name,
-                marker=dict(size=10, color=color),
-                showlegend=True,
-            )
-        )
-
-    return fig
+    :param fig: Figure Plotly à mettre à jour.
+    :type fig: go.Figure
+    :param title: Titre de la figure.
+    :type title: str
+    :param cell_color: Couleur de fond de la figure.
+    :type cell_color: str
+    :param annotations: Liste des annotations à ajouter à la figure.
+    :type annotations: list[dict]
+    :param legend: Dictionnaire de configuration de la légende.
+    :type legend: dict
+    """
+    fig.update_layout(
+        height=2800,
+        plot_bgcolor=cell_color,
+        # showlegend=True,
+        title=dict(
+            text=title,
+            font=dict(size=24, weight="bold"),
+            x=0.5,
+            y=0.99,
+            xanchor="center",
+        ),
+        xaxis_title=dict(text="Survey Order", font=dict(weight="bold")),
+        yaxis_title=dict(
+            text="Percentage of Soundings in Survey Order",
+            font=dict(weight="bold"),
+        ),
+        xaxis2_title=dict(text="Survey Order", font=dict(weight="bold")),
+        yaxis2_title=dict(
+            text="Depth (m)",
+            font=dict(weight="bold"),
+        ),
+        yaxis2=dict(autorange="reversed"),
+        xaxis3_title=dict(text="Survey Order", font=dict(weight="bold")),
+        yaxis3_title=dict(
+            text="TVU (m)",
+            font=dict(weight="bold"),
+        ),
+        yaxis3=dict(autorange="reversed"),
+        xaxis4_title=dict(text="Survey Order", font=dict(weight="bold")),
+        yaxis4_title=dict(
+            text="THU (m)",
+            font=dict(weight="bold"),
+        ),
+        yaxis4=dict(autorange="reversed"),
+        template="plotly",
+        annotations=annotations,
+        legend=legend,
+    )
 
 
 def plot_metadata(
@@ -195,7 +365,7 @@ def plot_metadata(
     df.columns = ["Metadata", "Value"]
 
     fig = make_subplots(
-        rows=6,
+        rows=7,
         cols=2,
         specs=[
             [{"type": "domain"}, {"type": "domain"}],
@@ -203,7 +373,8 @@ def plot_metadata(
             [None, {"type": "domain"}],
             [{"type": "xy", "rowspan": 2}, {"type": "domain"}],
             [None, {"type": "domain"}],
-            [None, {"type": "domain"}],
+            [{"type": "xy"}, {"type": "domain"}],
+            [{"type": "xy"}, None],
         ],
         subplot_titles=(
             "Metadata Table",
@@ -212,10 +383,15 @@ def plot_metadata(
             "",
             "",
             "Depths by Order",
+            "",
+            "",
+            "TVU by Order",
+            "",
+            "THU by Order",
         ),
         vertical_spacing=0.045,
         horizontal_spacing=0.05,
-        row_heights=[0.24, 0.19, 0.19, 0.19, 0.19, 0.19],
+        row_heights=[0.24, 0.19, 0.19, 0.19, 0.19, 0.19, 0.19],
         column_widths=[0.6, 0.4],
     )
 
@@ -228,52 +404,25 @@ def plot_metadata(
     for i, trace in enumerate(iho_order_fig.data, start=1):
         fig.add_trace(trace, row=i, col=2)
 
-    depth_fig = plot_depth_statistics(metadata["IHO Order Statistic"])
-    for trace in depth_fig.data:
-        fig.add_trace(trace, row=4, col=1)
+    def add_traces_to_figure(figure: go.Figure, row: int, col: int) -> None:
+        for trace_ in figure.data:
+            fig.add_trace(trace_, row=row, col=col)
 
-    # Mettre à jour la mise en page de la figure
-    fig.update_layout(
-        height=2200,
-        showlegend=True,
-        title=dict(
-            text=title,
-            font=dict(size=24, weight="bold"),
-            x=0.5,
-            y=0.99,
-            xanchor="center",
-        ),
-        xaxis_title=dict(text="Survey Order", font=dict(weight="bold")),
-        yaxis_title=dict(
-            text="Percentage of Soundings in Survey Order",
-            font=dict(weight="bold"),
-        ),
-        xaxis2_title=dict(text="Survey Order", font=dict(weight="bold")),
-        yaxis2_title=dict(
-            text="Depth (m)",
-            font=dict(weight="bold"),
-        ),
-        yaxis2=dict(autorange="reversed"),
-        template="plotly",
-        annotations=[
-            dict(
-                text=annotation["text"],
-                x=annotation["x"],
-                y=annotation["y"],
-                xanchor=annotation["xanchor"],
-                yanchor=annotation["yanchor"],
-                font=dict(size=20, weight="bold"),
-                showarrow=False,
-            )
-            for annotation in fig["layout"]["annotations"]
-        ],
-        legend=dict(
-            x=0.01,  # Position horizontale (1 = à droite, 0 = à gauche)
-            y=0.21,  # Position verticale (1 = en haut, 0 = en bas)
-            xanchor="left",
-            yanchor="top",
-            bgcolor="rgba(255, 255, 255, 0.7)",
-        ),
+    depth_fig = plot_depth_statistics(metadata["IHO Order Statistic"])
+    add_traces_to_figure(depth_fig, row=4, col=1)
+
+    tvu_fig = plot_tvu_statistics(metadata["IHO Order Statistic"])
+    add_traces_to_figure(tvu_fig, row=6, col=1)
+
+    thu_fig = plot_thu_statistics(metadata["IHO Order Statistic"])
+    add_traces_to_figure(thu_fig, row=7, col=1)
+
+    update_layout(
+        fig=fig,
+        title=title,
+        cell_color=CELL_COLOR,
+        annotations=create_annotations(fig["layout"]["annotations"]),
+        legend=create_legend(),
     )
 
     if show_plot:
