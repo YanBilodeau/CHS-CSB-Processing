ca3a79d08d01973ff52bc4ddddd2450b88a68fe3
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-03-14 14:24:00 -0400

    déplacement de DataCleaningFunctionError

--- src/transformation/exception_tranformation.py
+++ src/transformation/exception_tranformation.py
@@ -8,21 +8,6 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class DataCleaningFunctionError(Exception):
-    """
-    Exception levée lorsqu'une fonction de nettoyage n'existe pas.
-
-    :param function: (str) La fonction de nettoyage.
-    """
-
-    function: str
-    """La fonction de nettoyage."""
-
-    def __str__(self):
-        return f"La fonction de nettoyage '{self.function}' n'existe pas."
-
-
-@dataclass(frozen=True)
 class WaterLevelDataRequiredError(Exception):
     """
     Exception levée lorsqu'il manque les données de niveau d'eau.
