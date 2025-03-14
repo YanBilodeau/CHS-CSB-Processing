daf3920b8070d08c83edb67184b58b16c5b2262c
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-03-14 14:22:52 -0400

    déplacement de DataFilterConfigProtocol

--- src/transformation/transformation_models.py
+++ src/transformation/transformation_models.py
@@ -5,39 +5,7 @@ Ce module contient les modèles de données pour la transformation des données.
 """
 
 from datetime import datetime
-from typing import Protocol, Optional
-
-
-class DataFilterConfigProtocol(Protocol):
-    """
-    Protocole pour la configuration des filtres de données.
-
-    :param min_latitude: (int | float) La latitude minimale.
-    :param max_latitude: (int | float) La latitude maximale.
-    :param min_longitude: (int | float) La longitude minimale.
-    :param max_longitude: (int | float) La longitude maximale.
-    :param min_depth: (int | float) La profondeur minimale.
-    :param max_depth: (Optional[int | float]) La profonde maximale.
-    :param min_speed: (Optional[int | float]) La vitesse minimale.
-    :param max_speed: (Optional[int | float]) La vitesse maximale.
-    """
-
-    min_latitude: int | float
-    """La latitude minimale."""
-    max_latitude: int | float
-    """La latitude maximale."""
-    min_longitude: int | float
-    """La longitude minimale."""
-    max_longitude: int | float
-    """La longitude maximale."""
-    min_depth: int | float
-    """La profondeur minimale."""
-    max_depth: Optional[int | float]
-    """La profondeur maximale."""
-    min_speed: Optional[int | float]
-    """La vitesse minimale."""
-    max_speed: Optional[int | float]
-    """La vitesse maximale."""
+from typing import Protocol
 
 
 class SensorProtocol(Protocol):
