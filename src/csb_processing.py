87da9f2f878119a67596da885656e51d2c8f85f4
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-03-21 14:19:20 -0400

    changement version

--- src/csb_processing.py
+++ src/csb_processing.py
@@ -35,7 +35,7 @@ import transformation.georeference as georeference
 import vessel as vessel_manager
 
 
-__version__ = "0.7.0"
+__version__ = "0.6.1"
 
 
 LOGGER = logger.bind(name="CSB-Processing.WorkFlow")
