b18b4ab7ef34ca2a61ea5812a9019f70187fc91e
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-05-13 10:48:24 -0400

    ajout de FiltersEnum et FILTER_STATUS_MAPPING

--- src/filter/filter_models.py
+++ src/filter/filter_models.py
@@ -4,7 +4,10 @@ Module pour les modèles pour les filtres.
 Ce module contient les modèles de données pour les filtres des données.
 """
 
+from enum import StrEnum
 from typing import Protocol, Optional
+
+from schema import Status
 
 
 class DataFilterConfigProtocol(Protocol):
@@ -39,3 +42,24 @@ class DataFilterConfigProtocol(Protocol):
     """La vitesse maximale."""
     filter_to_apply: list[str]
     """Les filtres à appliquer."""
+
+
+class FiltersEnum(StrEnum):
+    """
+    Enum for status codes.
+    """
+
+    SPEED_FILTER = "SPEED_FILTER"
+    LATITUDE_FILTER = "LATITUDE_FILTER"
+    LONGITUDE_FILTER = "LONGITUDE_FILTER"
+    TIME_FILTER = "TIME_FILTER"
+    DEPTH_FILTER = "DEPTH_FILTER"
+
+
+FILTER_STATUS_MAPPING: dict[FiltersEnum, Status] = {
+    FiltersEnum.SPEED_FILTER: Status.REJECTED_BY_SPEED_FILTER,
+    FiltersEnum.LATITUDE_FILTER: Status.REJECTED_BY_LATITUDE_FILTER,
+    FiltersEnum.LONGITUDE_FILTER: Status.REJECTED_BY_LONGITUDE_FILTER,
+    FiltersEnum.TIME_FILTER: Status.REJECTED_BY_TIME_FILTER,
+    FiltersEnum.DEPTH_FILTER: Status.REJECTED_BY_DEPTH_FILTER,
+}
