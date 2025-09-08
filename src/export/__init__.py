6cbab4245d207a867efb61e74425f7045eecc207
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-09-08 15:55:12 -0400

    ajout d'import dans le init

--- src/export/__init__.py
+++ src/export/__init__.py
@@ -11,6 +11,7 @@ from .export_format import (
     export_geodataframe_to_geotiff,
 )
 from .factory_export import export_geodataframe, FileTypes
+from .export_helpers import finalize_geodataframe, get_export_file_name, split_data_by_iho_order, export_processed_data
 
 
 __all__ = [
@@ -22,4 +23,8 @@ __all__ = [
     "export_geodataframe_to_csar_api",
     "export_dataframe_to_csv",
     "export_geodataframe_to_geotiff",
+    "finalize_geodataframe",
+    "get_export_file_name",
+    "split_data_by_iho_order",
+    "export_processed_data",
 ]
--- src/tide/__init__.py
+++ src/tide/__init__.py
@@ -1,3 +1,7 @@
 """
 Ce package contient les classes et fonctions permettant de gérer les marées et leurs zones d'influences.
 """
+
+from . import stations, time_serie, voronoi, plot
+
+__all__ = ["stations", "time_serie", "voronoi", "plot"]
