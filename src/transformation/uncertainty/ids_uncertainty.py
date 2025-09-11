31e097d792fa9ff29e1047c7adeb60254da01872
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-09-11 13:52:02 -0400

    ajout constante

--- src/transformation/uncertainty/ids_uncertainty.py
+++ src/transformation/uncertainty/ids_uncertainty.py
@@ -4,3 +4,4 @@ UNCERTAINTY_PATH = Path(__file__).parent.parent.parent / "static" / "uncertainty
 STATION_UNCERTAINTY_JSON: Path = UNCERTAINTY_PATH / "station_uncertainty.json"
 SSP_ERRORS_PATH: Path = UNCERTAINTY_PATH / "canadian_waters_ssp_errors.gpkg"
 UNCERTAINTY_M: str = "uncertainty_m"
+SSP_ERROR_COEFFICIENT: str = "d_err_p"
