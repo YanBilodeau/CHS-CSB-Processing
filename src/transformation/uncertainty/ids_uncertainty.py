872297db95802aed959cdb7e677a23b79dd1c605
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-09-11 09:10:27 -0400

    changement de répertoire

--- src/transformation/uncertainty/ids_uncertainty.py
+++ src/transformation/uncertainty/ids_uncertainty.py
@@ -1,4 +1,6 @@
 from pathlib import Path
 
-STATION_UNCERTAINTY_JSON: Path = Path(__file__).parent / "station_uncertainty.json"
+UNCERTAINTY_PATH = Path(__file__).parent.parent.parent / "static" / "uncertainty"
+STATION_UNCERTAINTY_JSON: Path = UNCERTAINTY_PATH / "station_uncertainty.json"
+SSP_ERRORS_PATH: Path = UNCERTAINTY_PATH / "canadian_waters_ssp_errors.gpkg"
 UNCERTAINTY_M: str = "uncertainty_m"
