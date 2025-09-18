a8cd2b565e62bf36f1db5216bceeab8266ea65b0
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2025-09-18 10:51:51 -0400

    ajout import

--- src/tide/__init__.py
+++ src/tide/__init__.py
@@ -4,5 +4,13 @@ Ce package contient les classes et fonctions permettant de gérer les marées et
 
 from . import stations, time_serie, voronoi, plot
 from . import tide_zone_processing as tide_zone
+from . import water_level_export
 
-__all__ = ["stations", "time_serie", "voronoi", "plot", "tide_zone"]
+__all__ = [
+    "stations",
+    "time_serie",
+    "voronoi",
+    "plot",
+    "tide_zone",
+    "water_level_export",
+]
