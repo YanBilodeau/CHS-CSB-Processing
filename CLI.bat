6eefa47e7d0178c6f70e33189b4e73fd2745aed1
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2026-01-12 12:20:11 -0500

    Update CLI script to include water level station parameter and improve command clarity

--- CLI.bat
+++ CLI.bat
@@ -5,11 +5,12 @@ set "processing=%CD%\src\cli.py"
 echo %processing%
 
 rem "%python%" "%processing%" process --help
-
-"%python%" "%processing%" process D:\Dev\CHS-CSB_Processing\src\ingestion\Lowrance\Tuktoyaktuk --output D:\Dev\CHS-CSB_Processing\Output --excluded-station 06525 --excluded-station 03755
+rem "%python%" "%processing%" process D:\Dev\CHS-CSB_Processing\src\ingestion\Lowrance\Tuktoyaktuk --output D:\Dev\CHS-CSB_Processing\Output --excluded-station 06525 --excluded-station 03755
 rem --waterline 1.4 
 rem --apply-water-level False
-rem --vessel Tuktoyaktuk 
+rem --vessel Tuktoyaktuk
+
+"%python%" "%processing%" process D:\Dev\CHS-CSB_Processing\src\ingestion\Lowrance\Tuktoyaktuk --output D:\Dev\CHS-CSB_Processing\Output --water-level-station 06525
 
 rem "%python%" "%processing%" convert --help
 rem "%python%" "%processing%" convert D:\Dev\CHS-CSB_Processing\Output\Data\CH-Lowrance-unknown-20220813-20221105.geojson D:\Dev\CHS-CSB_Processing\Output\Data\CH-Lowrance-unknown-20220813-20221105.gpkg --output D:\Dev\CHS-CSB_Processing\Output\Convert --format csar --format geotiff --format geojson
