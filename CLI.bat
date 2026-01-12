0785d1156ef9dadbec16e0fdade543515edd0baf
BilodeauY <yan.bilodeau@dfo-mpo.gc.ca>
2026-01-12 12:16:33 -0500

    Fix CLI script to use singular form for excluded station parameters

--- CLI.bat
+++ CLI.bat
@@ -6,13 +6,12 @@ echo %processing%
 
 rem "%python%" "%processing%" process --help
 
-"%python%" "%processing%" process D:\Dev\CHS-CSB_Processing\src\ingestion\Lowrance\Tuktoyaktuk --output D:\Dev\CHS-CSB_Processing\Output --excluded-stations 06525 --excluded-stations 03755
+"%python%" "%processing%" process D:\Dev\CHS-CSB_Processing\src\ingestion\Lowrance\Tuktoyaktuk --output D:\Dev\CHS-CSB_Processing\Output --excluded-station 06525 --excluded-station 03755
 rem --waterline 1.4 
 rem --apply-water-level False
 rem --vessel Tuktoyaktuk 
 
 rem "%python%" "%processing%" convert --help
-
 rem "%python%" "%processing%" convert D:\Dev\CHS-CSB_Processing\Output\Data\CH-Lowrance-unknown-20220813-20221105.geojson D:\Dev\CHS-CSB_Processing\Output\Data\CH-Lowrance-unknown-20220813-20221105.gpkg --output D:\Dev\CHS-CSB_Processing\Output\Convert --format csar --format geotiff --format geojson
 
 pause
